2f4d94c0d0bbc8ef785f557abec945cce5681a45
NicolasReyAlonso <nicolasreyalonso@gmail.com>
2025-11-29 11:35:05 +0000

    ADD menu sound effects

--- BrainWallGodot/brain-wall/main_menu.gd
+++ BrainWallGodot/brain-wall/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
-
-
+@export var soundEffectHover: AudioStreamPlayer
+@export var soundEffectclick: AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,10 +12,25 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
+	soundEffectclick.pitch_scale = 1
+	soundEffectclick.play()
 	get_tree().change_scene_to_file("res://scenes/mainScene.tscn")
 	pass # Replace with function body.
 
 
 func _on_play_button_2_pressed() -> void:
+	soundEffectclick.pitch_scale = 0.8
+	soundEffectclick.play()
 	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_play_button_mouse_entered() -> void:
+	soundEffectHover.pitch_scale = 1
+	soundEffectHover.play()
+	pass # Replace with function body.
+
+func _on_play_button_2_mouse_entered() -> void:
+	soundEffectHover.pitch_scale = 0.8
+	soundEffectHover.play()
 	pass # Replace with function body.
